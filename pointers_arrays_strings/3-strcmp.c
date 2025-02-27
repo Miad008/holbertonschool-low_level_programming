462922aee530c93bff289c76674d02e94717bcdd
Miad008 <10694@holbertonstudents.com>
2025-02-27 18:19:01 +0000

    k

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @src: The source string.
  * @n: The maximum number of bytes to copy.
  *
- * Return: Pointer to the resulting string `dest`.
+ * Return: Pointer to `dest`.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -15,11 +15,8 @@ int i;
 for (i = 0; i < n && src[i] != '\0'; i++)
 dest[i] = src[i];
 
-while (i < n)
-{
+for (; i < n; i++)
 dest[i] = '\0';
-i++;
-}
 
 return (dest);
 }
