eeae658cd5fe744e35e7f04a080dbd96d85f9958
Miad008 <10694@holbertonstudents.com>
2025-02-27 16:30:51 +0000

    k

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string in place.
+ * @s: Pointer to the string.
+ */
+void rev_string(char *s)
+{
+int len = 0, i;
+char temp;
+while (s[len])
+len++;
+for (i = 0; i < len / 2; i++)
+{
+temp = s[i];
+s[i] = s[len - 1 - i];
+s[len - 1 - i] = temp;
+}
+}
