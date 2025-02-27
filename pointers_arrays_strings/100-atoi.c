c894b5a26cd8c49612fe47f605b7b3b070950a09
Miad008 <10694@holbertonstudents.com>
2025-02-27 17:43:57 +0000

    Fixed 100-atoi and cleaned up code

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * _atoi - Converts a string to an integer.
@@ -11,26 +10,22 @@ int _atoi(char *s)
 {
 int i = 0, sign = 1;
 unsigned int result = 0;
+int number_found = 0;
 
-while (s[i] && (s[i] < '0' || s[i] > '9'))
+while (s[i])
 {
 if (s[i] == '-')
 sign *= -1;
-i++;
-}
 
-while (s[i] >= '0' && s[i] <= '9')
+if (s[i] >= '0' && s[i] <= '9')
 {
-if (result > (unsigned int)INT_MAX / 10 ||
-	(result == (unsigned int)INT_MAX / 10 &&
-	(s[i] - '0') > (unsigned int)INT_MAX % 10))
-{
-return (sign == 1 ? INT_MAX : INT_MIN);
-}
-
 result = result * 10 + (s[i] - '0');
+number_found = 1;
+}
+else if (number_found)
+break;
 i++;
 }
 
-return (sign * (int)result);
+return (sign *result);
 }
