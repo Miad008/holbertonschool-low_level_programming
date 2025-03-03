ab0c178130e2b1996dee4485849da1829e473685
Miad008 <10694@holbertonstudents.com>
2025-03-03 21:07:58 +0000

    k

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,22 @@
  */
 void print_number(int n)
 {
+if (n == -2147483648)
+{
+_putchar('-');
+_putchar('2');
+_putchar('1');
+_putchar('4');
+_putchar('7');
+_putchar('4');
+_putchar('8');
+_putchar('3');
+_putchar('6');
+_putchar('4');
+_putchar('8');
+return;
+}
+
 if (n < 0)
 {
 _putchar('-');
