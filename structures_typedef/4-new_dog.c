756f261ce0fb72bbf12326cec4b443d8919e52fb
Miad008 <10694@holbertonstudents.com>
2025-03-14 20:39:20 +0000

    k

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,41 +1,90 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: pointer to the new dog (dog_t), or NULL if it fails
- */
+* new_dog - Creates a new dog
+* @name: Name of the dog
+* @age: Age of the dog
+* @owner: Owner of the dog
+* Return: Pointer to the new dog (dog_t), or NULL if it fails
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog;
+dog_t *max_dog;
+int name_l = 0, own_l = 0;
 
-new_dog = malloc(sizeof(dog_t));
-if (!new_dog)
+if (name == NULL || owner == NULL)
 return (NULL);
 
-new_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
-if (!new_dog->name)
+name_l = _strlen(name) + 1;
+own_l = _strlen(owner) + 1;
+
+/* Allocate memory for the dog structure */
+max_dog = malloc(sizeof(dog_t));
+if (max_dog == NULL)
+return (NULL);
+
+/* Allocate memory for name */
+max_dog->name = malloc(sizeof(char) * name_l);
+if (max_dog->name == NULL)
 {
-free(new_dog);
+free(max_dog);
 return (NULL);
 }
 
-new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-if (!new_dog->owner)
+/* Allocate memory for owner */
+max_dog->owner = malloc(sizeof(char) * own_l);
+if (max_dog->owner == NULL)
 {
-free(new_dog->name);
-free(new_dog);
+free(max_dog->name);
+free(max_dog);
 return (NULL);
 }
 
-strcpy(new_dog->name, name);
-new_dog->age = age;
-strcpy(new_dog->owner, owner);
+/* Copy name and owner */
+_strcpy(max_dog->name, name);
+_strcpy(max_dog->owner, owner);
 
-return (new_dog);
+/* Initialize age */
+max_dog->age = age;
+
+return (max_dog);
+}
+
+/**
+* _strlen - Returns the length of a string
+* @s: String to count
+* Return: Length of the string
+*/
+int _strlen(char *s)
+{
+int c = 0;
+
+for (; *s != '\0'; s++)
+{
+c++;
+}
+return (c);
+}
+
+/**
+* _strcpy - Copies a string
+* @dest: Destination value
+* @src: Source value
+* Return: Pointer to dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
+
+return (dest);
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,26 +1,8 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * struct dog - Structure representing a dog
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
- */
-struct dog
-{
-char *name;
-float age;
-char *owner;
-};
-
-
-typedef struct dog dog_t;
-
-
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
