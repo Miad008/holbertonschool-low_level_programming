eb9a5a61402a2a231c8f61fcc8a28852b26f8f58
Miad008 <10694@holbertonstudents.com>
2025-03-14 20:42:23 +0000

    k

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,30 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
-int _putchar(char c);
+#include <stddef.h> /* For NULL */
+
+/**
+* struct dog - structure to store a dog's information
+* @name: the name of the dog
+* @age: the age of the dog
+* @owner: the owner of the dog
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 void print_dog(struct dog *d);
+
+
 
 #endif
