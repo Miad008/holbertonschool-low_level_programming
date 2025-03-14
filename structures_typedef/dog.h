34c713ed561743b252b678b4a68a9208782e50f3
Miad008 <10694@holbertonstudents.com>
2025-03-14 19:58:02 +0000

    k

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,21 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - structure to store dog information
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
- */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
